26f075c52e6e7bbca61f94146b12229042b23f01
masc01 <masc01@2252c265-7fd8-49a5-b10e-6a9b7765d42e>
2008-08-22 15:29:38 +0000

    a dummy feature extractor which sends some random values every 10 ms. untested.
    git-svn-id: https://semaine.opendfki.de/repos/trunk@10 2252c265-7fd8-49a5-b10e-6a9b7765d42e

---
+++ java/src/eu/semaine/components/dummy/DummyFeatureExtractor.java
@@ -0,0 +1,51 @@
+/**
+ * Copyright (C) 2008 DFKI GmbH. All rights reserved.
+ * Use is subject to license terms -- see license.txt.
+ */
+package eu.semaine.components.dummy;
+
+import java.util.Random;
+
+import javax.jms.JMSException;
+
+import eu.semaine.components.Component;
+import eu.semaine.jms.FeatureSender;
+import eu.semaine.jms.Sender;
+
+/**
+ * @author marc
+ *
+ */
+public class DummyFeatureExtractor extends Component 
+{
+	private FeatureSender featureSender;
+	/**
+	 * @param componentName
+	 * @throws JMSException
+	 */
+	public DummyFeatureExtractor() throws JMSException 
+	{
+		super("DummyFeatureExtractor");
+		int period = 10; // ms
+		featureSender = new FeatureSender("semaine.data.analysis.dummy", "", getName(), period);
+		waitingTime = period;
+		senders.add(featureSender); // so it can be started etc
+		featureSender.setFeatureNames(new String[] {
+			"f0.level",
+			"f0.range",
+			"energy.db",
+			"optical.flow",
+			"facialfeaturepoint1.x"
+		});
+	}
+
+	public void act() throws JMSException
+	{
+		float[] features = new float[featureSender.getFeatureNames().length];
+		Random random = new Random();
+		for (int i=0; i<features.length; i++) {
+			features[i] = random.nextFloat();
+		}
+		featureSender.sendFeatureVector(features, System.currentTimeMillis());
+	}
+}
