455c90560a0ddc77f4a1ecccc6da9a0bb567eaff
masc01 <masc01@2252c265-7fd8-49a5-b10e-6a9b7765d42e>
2008-08-25 14:19:35 +0000

    basic support for receiving EMMA messages
    git-svn-id: https://semaine.opendfki.de/repos/trunk@15 2252c265-7fd8-49a5-b10e-6a9b7765d42e

---
+++ java/src/eu/semaine/jms/EmmaReceiver.java
@@ -0,0 +1,50 @@
+/**
+ * Copyright (C) 2008 DFKI GmbH. All rights reserved.
+ * Use is subject to license terms -- see license.txt.
+ */
+package eu.semaine.jms;
+
+import javax.jms.JMSException;
+import javax.jms.Message;
+
+import org.w3c.dom.Element;
+
+import eu.semaine.datatypes.SEMAINEEmmaMessage;
+import eu.semaine.datatypes.SEMAINEMessage;
+import eu.semaine.datatypes.SEMAINEXMLMessage;
+import eu.semaine.exceptions.MessageFormatException;
+
+/**
+ * @author marc
+ *
+ */
+public class EmmaReceiver extends XMLReceiver {
+
+	/**
+	 * @param topicName
+	 * @throws JMSException
+	 */
+	public EmmaReceiver(String topicName) throws JMSException {
+		super(topicName);
+	}
+
+	/**
+	 * @param topicName
+	 * @param messageSelector
+	 * @throws JMSException
+	 */
+	public EmmaReceiver(String topicName, String messageSelector)
+			throws JMSException {
+		super(topicName, messageSelector);
+	}
+	
+	
+	@Override
+	protected SEMAINEMessage createSEMAINEMessage(Message message)
+	throws MessageFormatException
+	{
+		return new SEMAINEEmmaMessage(message);
+	}
+
+
+}
