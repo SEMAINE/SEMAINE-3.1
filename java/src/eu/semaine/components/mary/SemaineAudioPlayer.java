c511e402e1308d597d05957becaa70f6da3e4bd8
sach01 <sach01@2252c265-7fd8-49a5-b10e-6a9b7765d42e>
2008-11-18 15:44:57 +0000

    Semaine Audioplayer
    git-svn-id: https://semaine.opendfki.de/repos/trunk@114 2252c265-7fd8-49a5-b10e-6a9b7765d42e

---
+++ java/src/eu/semaine/components/mary/SemaineAudioPlayer.java
@@ -0,0 +1,114 @@
+/**
+ * Copyright (C) 2008 DFKI GmbH. All rights reserved.
+ * Use is subject to license terms -- see license.txt.
+ */
+package eu.semaine.components.mary;
+
+import java.io.ByteArrayInputStream;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.TimeUnit;
+
+import javax.jms.JMSException;
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioInputStream;
+
+import marytts.util.data.audio.AudioPlayer;
+import eu.semaine.components.Component;
+import eu.semaine.jms.message.SEMAINEBytesMessage;
+import eu.semaine.jms.message.SEMAINEMessage;
+import eu.semaine.jms.receiver.BytesReceiver;
+
+/**
+ * Audio player
+ * @author Sathish Chandra Pammi
+ *
+ */
+public class SemaineAudioPlayer extends Component 
+{
+	private BytesReceiver audioReceiver;
+	private BlockingQueue<byte[]> inputWaiting;
+	Playloop player;
+	
+	/**
+	 * @param componentName
+	 * @throws JMSException
+	 */
+	public SemaineAudioPlayer() throws JMSException 
+	{
+		super("SeamineAudioPlayer");
+		audioReceiver = new BytesReceiver("semaine.data.lowlevel.audio");
+		receivers.add(audioReceiver); // to set up properly
+		inputWaiting = new LinkedBlockingQueue<byte[]>();
+		player =  new Playloop(inputWaiting);
+	}
+	
+	@Override
+	public void react(SEMAINEMessage m) throws JMSException
+	{
+		if(m.isBytesMessage() && m.getTopicName().equals("semaine.data.lowlevel.audio")){
+			SEMAINEBytesMessage bm = (SEMAINEBytesMessage)m;
+			byte[] audioData =  bm.getBytes();
+			try {
+				inputWaiting.put(audioData);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		
+	}
+	
+	public class Playloop extends Thread{
+		
+		protected BlockingQueue<byte[]> inputWaiting;
+		protected boolean playing = false;
+		
+		public Playloop(BlockingQueue<byte[]> inputWaiting){
+			this.inputWaiting = inputWaiting;	
+			start();
+		}
+		
+		public void run(){
+			for (;;)  {  
+				byte[] audioData = null;
+				try {
+					// block until input becomes available
+					audioData = inputWaiting.poll(waitingTime, TimeUnit.MILLISECONDS);
+					if(audioData == null) continue;
+				} catch (InterruptedException ie) {
+					// if we have no input, we'll keep on waiting
+					continue;
+				}
+				ByteArrayInputStream bais = new ByteArrayInputStream(audioData);
+		        boolean signed = true; //true,false
+		        int bitsPerSample = 16;
+		        int samplingRate = 16000;
+		        int channels = 1;
+		        boolean bigEndian = false; 
+		        long lengthInSamples = audioData.length / (bitsPerSample/8);
+		        
+		        AudioFormat af = new AudioFormat(
+		        samplingRate,
+		        bitsPerSample,
+		        channels,
+		        signed,
+		        bigEndian);
+		        
+				AudioInputStream ais = new AudioInputStream(bais, af, lengthInSamples);
+				
+				try {
+	            	AudioPlayer player = new AudioPlayer(ais);
+					player.start();
+					player.join();
+					sleep(1000);
+					//this.wait(300);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				} 
+			}
+		}
+		
+	}
+		
+	
+}
