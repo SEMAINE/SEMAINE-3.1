778776cc27feb630bbb66cddce854c61c92f631f
sach01 <sach01@2252c265-7fd8-49a5-b10e-6a9b7765d42e>
2008-11-18 15:43:26 +0000

    Dummy VisualFML2BML component
    git-svn-id: https://semaine.opendfki.de/repos/trunk@113 2252c265-7fd8-49a5-b10e-6a9b7765d42e

---
+++ java/src/eu/semaine/components/dummy/DummyVisualFML2BML.java
@@ -0,0 +1,68 @@
+/**
+ * Copyright (C) 2008 DFKI GmbH. All rights reserved.
+ * Use is subject to license terms -- see license.txt.
+ */
+package eu.semaine.components.dummy;
+
+import javax.jms.JMSException;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
+import eu.semaine.components.Component;
+import eu.semaine.datatypes.xml.BML;
+import eu.semaine.exceptions.MessageFormatException;
+import eu.semaine.jms.message.SEMAINEMessage;
+import eu.semaine.jms.message.SEMAINEXMLMessage;
+import eu.semaine.jms.receiver.BMLReceiver;
+import eu.semaine.jms.receiver.FMLReceiver;
+import eu.semaine.jms.sender.BMLSender;
+import eu.semaine.util.XMLTool;
+
+/**
+ * This dummy action selection accepts all candidates.
+ * 
+ * @author marc
+ *
+ */
+public class DummyVisualFML2BML extends Component 
+{
+	private FMLReceiver fmlReceiver;
+	private BMLSender bmlSender;
+	
+	
+
+	/**
+	 * @param componentName
+	 * @throws JMSException
+	 */
+	public DummyVisualFML2BML() throws JMSException 
+	{
+		super("DummyFML2BML");
+		fmlReceiver = new FMLReceiver("semaine.data.action.selected.speechpreprocessed");
+		receivers.add(fmlReceiver); // to set up properly
+		
+		bmlSender = new BMLSender("semaine.data.synthesis.plan", getName());
+		senders.add(bmlSender); // so it can be started etc
+	}
+	
+
+	@Override
+	public void react(SEMAINEMessage m) throws JMSException
+	{
+		if (!(m instanceof SEMAINEXMLMessage)) {
+			throw new MessageFormatException("expected XML message, got "+m.getClass().getSimpleName());
+		}
+		SEMAINEXMLMessage xm = (SEMAINEXMLMessage)m;
+		boolean isFML = "FML".equals(xm.getDatatype());
+		if (isFML) {
+			// extract the BML and send that along
+			Element bml = XMLTool.needChildElementByTagNameNS(
+					xm.getDocument().getDocumentElement(), BML.BML, BML.namespace);
+			Document doc = XMLTool.newDocument(BML.BML, BML.namespace);
+			doc.adoptNode(bml);
+			doc.replaceChild(bml, doc.getDocumentElement());
+			bmlSender.sendXML(doc, xm.getUsertime(), xm.getEventType());
+		} 
+	}
+}
