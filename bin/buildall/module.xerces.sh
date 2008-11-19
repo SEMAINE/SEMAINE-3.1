52b990448419f1f69ef0ef6cd0d3b59d924d2036
florian.eyben <florian.eyben@2252c265-7fd8-49a5-b10e-6a9b7765d42e>
2008-11-19 13:01:43 +0000

    fixed a syntax error in xerces build module
    git-svn-id: https://semaine.opendfki.de/repos/trunk@124 2252c265-7fd8-49a5-b10e-6a9b7765d42e

--- bin/buildall/module.xerces.sh
+++ bin/buildall/module.xerces.sh
@@ -14,14 +14,14 @@ function func_build_xerces {
     export XERCESCROOT=$PWD
     cd src/xercesc
 
-    if test "x$BUILDSYSTEM" = "xcygwin" then
+    if test "x$BUILDSYSTEM" = "xcygwin" ; then
       ./runConfigure -p cygwin -P $INSTALL_PREFIX
-    elif test "x$BUILDSYSTEM" = "xmacosx" then
+    elif test "x$BUILDSYSTEM" = "xmacosx" ; then
       ./runConfigure -p macosx -P $INSTALL_PREFIX
     else
       ./runConfigure -p linux -P $INSTALL_PREFIX
     fi
-    
+
     if test "x$?" != "x0" ; then
       return 1;
     fi
