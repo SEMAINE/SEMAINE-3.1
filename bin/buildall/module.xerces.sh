791c7119c69a5bbe764ee8af8bb9be22aeb92797
masc01 <masc01@2252c265-7fd8-49a5-b10e-6a9b7765d42e>
2009-07-27 18:42:24 +0000

    Use Xerces 3 now. Seems like the build process has been simplified; tested on a Mac, works fine; needs testing on Linux (and cygwin?).
    git-svn-id: https://semaine.opendfki.de/repos/trunk@322 2252c265-7fd8-49a5-b10e-6a9b7765d42e

--- bin/buildall/module.xerces.sh
+++ bin/buildall/module.xerces.sh
@@ -1,8 +1,8 @@
 
 #################### Package Configuration    #########################
-XERCES_BASE="xerces-c-src_2_8_0"
+XERCES_BASE="xerces-c-3.0.1"
 
-XERCES_URL="http://apache.linux-mirror.org/xerces/c/2/sources/$XERCES_BASE.tar.gz"
+XERCES_URL="http://apache.linux-mirror.org/xerces/c/3/sources/$XERCES_BASE.tar.gz"
 
 register_build "xerces-c" "$XERCES_URL" "$XERCES_BASE" "func_build_xerces" $1
 
@@ -10,17 +10,20 @@ register_build "xerces-c" "$XERCES_URL" "$XERCES_BASE" "func_build_xerces" $1
 
 # build_nr var must be set before this function is called
 function func_build_xerces {
-  
-    export XERCESCROOT=$PWD
-    cd src/xercesc
 
-    if test "x$BUILDSYSTEM" = "xcygwin" ; then
-      ./runConfigure -p cygwin -P $INSTALL_PREFIX
-    elif test "x$BUILDSYSTEM" = "xmacosx" ; then
-      ./runConfigure -p macosx -P $INSTALL_PREFIX
-    else
-      ./runConfigure -p linux -P $INSTALL_PREFIX
-    fi
+    ./configure --prefix=$INSTALL_PREFIX 
+
+# For xerces 2.8:    
+#    export XERCESCROOT=$PWD
+#    cd src/xercesc
+
+#    if test "x$BUILDSYSTEM" = "xcygwin" ; then
+#      ./runConfigure -p cygwin -P $INSTALL_PREFIX
+#    elif test "x$BUILDSYSTEM" = "xmacosx" ; then
+#      ./runConfigure -p macosx -P $INSTALL_PREFIX
+#    else
+#      ./runConfigure -p linux -P $INSTALL_PREFIX
+#    fi
 
     if test "x$?" != "x0" ; then
       return 1;
