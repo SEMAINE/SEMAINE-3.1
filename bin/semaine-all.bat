9bfe18041e5f34c610d7c50b6b59de849ba6a90e
etienne.desevin <etienne.desevin@2252c265-7fd8-49a5-b10e-6a9b7765d42e>
2009-10-20 14:47:06 +0000

    bat files for starting and stopping all Greta Modules
    git-svn-id: https://semaine.opendfki.de/repos/trunk@427 2252c265-7fd8-49a5-b10e-6a9b7765d42e

---
+++ bin/semaine-all.bat
@@ -0,0 +1,14 @@
+start /MIN .\..\GretaActive\bin\Active_ActionSelectionDebug.exe
+..\GretaActive\bin\utils\WAIT.exe 1
+
+start /MIN .\..\GretaActive\bin\Active_BehaviorPlannerDebug.exe
+..\GretaActive\bin\utils\WAIT.exe 1
+
+start /MIN .\..\GretaActive\bin\Active_BehaviorRealizerDebug.exe
+..\GretaActive\bin\utils\WAIT.exe 1
+
+start /MIN .\..\GretaActive\bin\Active_ListenerIntentPlannerDebug.exe
+..\GretaActive\bin\utils\WAIT.exe 1
+
+start .\..\GretaActive\bin\Active_PlayerDebug.exe
+..\GretaActive\bin\utils\WAIT.exe 1
---
+++ bin/semaine-stop-all.bat
@@ -0,0 +1,6 @@
+taskkill /f /im Active_ActionSelectionDebug.exe
+taskkill /f /im Active_BehaviorPlannerDebug.exe
+taskkill /f /im Active_BehaviorRealizerDebug.exe
+taskkill /f /im Active_ListenerIntentPlannerDebug.exe
+taskkill /f /im Active_PlayerDebug.exe
+
