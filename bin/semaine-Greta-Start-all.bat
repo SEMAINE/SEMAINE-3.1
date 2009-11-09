1ae091fbc3b51351d2df7004966c3c8be64c7aa8
semaine <semaine@2252c265-7fd8-49a5-b10e-6a9b7765d42e>
2009-11-09 08:39:49 +0000

    extracted GRETA instlalation dir as environment variable, for easier customisation
    git-svn-id: https://semaine.opendfki.de/repos/trunk@434 2252c265-7fd8-49a5-b10e-6a9b7765d42e

--- bin/semaine-Greta-Start-all.bat
+++ bin/semaine-Greta-Start-all.bat
@@ -1,14 +1,18 @@
-start /MIN .\..\GretaActive\bin\Active_ActionSelectionDebug.exe
-..\GretaActive\bin\utils\WAIT.exe 1
+:: Start all Greta components with one script
 
-start /MIN .\..\GretaActive\bin\Active_BehaviorPlannerDebug.exe
-..\GretaActive\bin\utils\WAIT.exe 1
+:: Update this to match the location of your GRETA installation, if needed:
+set GRETADIR="..\GretaActive"
+start /MIN .\%GRETADIR%\bin\Active_ActionSelectionDebug.exe
+%GRETADIR%\bin\utils\WAIT.exe 1
 
-start /MIN .\..\GretaActive\bin\Active_BehaviorRealizerDebug.exe
-..\GretaActive\bin\utils\WAIT.exe 1
+start /MIN .\%GRETADIR%\bin\Active_BehaviorPlannerDebug.exe
+%GRETADIR%\bin\utils\WAIT.exe 1
 
-start /MIN .\..\GretaActive\bin\Active_ListenerIntentPlannerDebug.exe
-..\GretaActive\bin\utils\WAIT.exe 1
+start /MIN .\%GRETADIR%\bin\Active_BehaviorRealizerDebug.exe
+%GRETADIR%\bin\utils\WAIT.exe 1
 
-start .\..\GretaActive\bin\Active_PlayerDebug.exe
-..\GretaActive\bin\utils\WAIT.exe 1
+start /MIN .\%GRETADIR%\bin\Active_ListenerIntentPlannerDebug.exe
+%GRETADIR%\bin\utils\WAIT.exe 1
+
+start .\%GRETADIR%\bin\Active_PlayerDebug.exe
+%GRETADIR%\bin\utils\WAIT.exe 1
