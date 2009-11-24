44a098d1b4aab599ab4901a534dad3c162065482
florian.eyben <florian.eyben@2252c265-7fd8-49a5-b10e-6a9b7765d42e>
2009-11-24 18:29:48 +0000

    added openSMILE windows build instructions
    git-svn-id: https://semaine.opendfki.de/repos/trunk@446 2252c265-7fd8-49a5-b10e-6a9b7765d42e

---
+++ c++/src/tum/auxiliary/build/copy_libs.bat
@@ -0,0 +1,24 @@
+
+REM This bat files copies libraries required by openSMILE to the openSMILE root directory
+
+set rootpath=..\..\..\..\..
+set TP=%rootpath%\thirdparty\build
+set target=..\..\opensmile\
+set APRlibs=C:\Program Files\Apache Software Foundation\Apache2.2
+
+
+REM Release libraries:
+copy /Y %TP%\portaudio-win\portaudio_x86Release.dll %target%
+copy /Y %TP%\xerces-c-src\Build\Win32\VC8\Release\xerces-c_3_0.dll
+copy /Y %TP%\xqilla\build\windows\VC8\Win32\Release\xqilla22.dll
+
+REM Debug libraries:
+copy /Y %TP%\portaudio-win\portaudio_x86Debug.dll %target%
+copy /Y %TP%\xerces-c-src\Build\Win32\VC8\Debug\xerces-c_3_0D.dll %target%
+copy /Y %TP%\xqilla\build\windows\VC8\Win32\Debug\xqilla22d.dll %target%
+
+REM APR runtime:
+copy /Y %APRlibs%\libapr-1.dll %target%
+copy /Y %APRlibs%\libapriconv-1.dll %target%
+copy /Y %APRlibs%\libaprutil-1.dll %target%
+
