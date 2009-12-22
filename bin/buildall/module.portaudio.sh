4e97843b827ceb691ee11102cd924d2fea5cfbe3
florian.eyben <florian.eyben@2252c265-7fd8-49a5-b10e-6a9b7765d42e>
2009-12-22 10:26:17 +0000

    fixed a file confusion in portaudio linux build script
    git-svn-id: https://semaine.opendfki.de/repos/trunk@512 2252c265-7fd8-49a5-b10e-6a9b7765d42e

--- bin/buildall/module.portaudio.sh
+++ bin/buildall/module.portaudio.sh
@@ -9,7 +9,7 @@ register_build "portaudio" "$PA_URL" "$PA_BASE" "func_build_portaudio" $1
 #######################################################################
 
 if [ -e "$SEMAINE_ROOT/bin/buildall/portaudio_working.tgz" ]; then
-  cp $SEMAINE_ROOT/bin/buildall/portaudio_working.tgz $DOWNLOAD_PREFIX/portaudio.tgz
+  cp $SEMAINE_ROOT/bin/buildall/portaudio_working.tgz $DOWNLOAD_PREFIX/pa_snapshot.tgz
 fi
 
 # build_nr var must be set before this function is called
