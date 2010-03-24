19e01383145f57605c547fde7de5459ddb157bb6
florian.eyben <florian.eyben@2252c265-7fd8-49a5-b10e-6a9b7765d42e>
2010-03-24 16:58:32 +0000

    updated opensmileSemaine.conf to run with latest svn version, added loading of generic pre-trained voice model for the VAD. Included the voice and silence model files. Added an example keyword weights file. Added some more pronunciations to the dictionary. Added HELLO to the dictionary, to avoid detecting "HELL" if the user says "HELLO".
    git-svn-id: https://semaine.opendfki.de/repos/trunk@526 2252c265-7fd8-49a5-b10e-6a9b7765d42e

--- c++/src/tum/auxiliary/build/copy_libs.bat
+++ c++/src/tum/auxiliary/build/copy_libs.bat
@@ -3,19 +3,22 @@ REM This bat files copies libraries required by openSMILE to the openSMILE root 
 
 set rootpath=..\..\..\..\..
 set TP=%rootpath%\thirdparty\build
-set target=..\..\opensmile\
+set target=..\..\opensmile
+REM set target=..\..\opensmile\bin
 set APRlibs=C:\Program Files\Apache Software Foundation\Apache2.2
 
 
 REM Release libraries:
 copy /Y %TP%\portaudio-win\portaudio_x86Release.dll %target%
-copy /Y %TP%\xerces-c-src\Build\Win32\VC8\Release\xerces-c_3_0.dll
-copy /Y %TP%\xqilla\build\windows\VC8\Win32\Release\xqilla22.dll
+copy /Y %TP%\xerces-c-src\Build\Win32\VC8\Release\xerces-c_3_0.dll %target%
+copy /Y %TP%\xerces-c-src\Build\Win32\VC8\Release\xerces-c_3_1.dll %target%
+copy /Y %TP%\xqilla\build\windows\VC8\Win32\Release\xqilla22.dll %target%
 
 REM Debug libraries:
-copy /Y %TP%\portaudio-win\portaudio_x86Debug.dll %target%
-copy /Y %TP%\xerces-c-src\Build\Win32\VC8\Debug\xerces-c_3_0D.dll %target%
-copy /Y %TP%\xqilla\build\windows\VC8\Win32\Debug\xqilla22d.dll %target%
+ copy /Y %TP%\portaudio-win\portaudio_x86Debug.dll %target%
+ copy /Y %TP%\xerces-c-src\Build\Win32\VC8\Debug\xerces-c_3_0D.dll %target%
+ copy /Y %TP%\xerces-c-src\Build\Win32\VC8\Debug\xerces-c_3_1D.dll %target%
+ copy /Y %TP%\xqilla\build\windows\VC8\Win32\Debug\xqilla22d.dll %target%
 
 REM APR runtime:
 copy /Y %APRlibs%\libapr-1.dll %target%
