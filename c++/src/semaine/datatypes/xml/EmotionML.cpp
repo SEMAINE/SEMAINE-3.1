17e62360aa4055a05305b5d1bb57c138b77aa26c
masc01 <masc01@2252c265-7fd8-49a5-b10e-6a9b7765d42e>
2010-08-25 14:22:03 +0000

    BREAKING CHANGE! Code using EmotionML must be changed so it compiles again.
    Updating EmotionML.java to be in line with the Second Public Working Draft of EmotionML, http://www.w3.org/TR/2010/WD-emotionml-20100729/
    
    This version removes support for old Elements such as <dimensions> and old Attributes such as set, and adds a complete list of element and attribute names from the latest working draft.
    
    The file also contains string constants for a number of Emotion vocabularies relevant in SEMAINE, and convenience functions for converting value ranges between SEMAINE dimensions and FSRE dimensions.
    
    git-svn-id: https://semaine.opendfki.de/repos/trunk@564 2252c265-7fd8-49a5-b10e-6a9b7765d42e

--- c++/src/semaine/datatypes/xml/EmotionML.cpp
+++ c++/src/semaine/datatypes/xml/EmotionML.cpp
@@ -13,28 +13,77 @@ namespace semaine {
 namespace datatypes {
 namespace xml {
 
-const std::string EmotionML::version = "0.1";
-const std::string EmotionML::namespaceURI = "http://www.w3.org/2005/Incubator/emotion";
+const std::string EmotionML::namespaceURI = "http://www.w3.org/2009/10/emotionml";
+const std::string EmotionML::version = "1.0-WD20100729";
 	
-	// Elements
+// Elements
+const std::string EmotionML::E_EMOTIONML = "emotionml";
 const std::string EmotionML::E_EMOTION = "emotion";
-const std::string EmotionML::E_ROOT_TAGNAME = "emotion";
+const std::string EmotionML::E_ROOT_TAGNAME = "emotionml";
 const std::string EmotionML::E_CATEGORY = "category";
-const std::string EmotionML::E_DIMENSIONS = "dimensions";
-const std::string EmotionML::E_AROUSAL = "arousal";
-const std::string EmotionML::E_VALENCE = "valence";
-//const std::string EmotionML::E_POTENCY = "potency";
-const std::string EmotionML::E_POWER = "power";
+const std::string EmotionML::E_DIMENSION = "dimension";
+const std::string EmotionML::E_APPRAISAL = "appraisal";
+const std::string EmotionML::E_ACTION_TENDENCY = "action-tendency";
 const std::string EmotionML::E_INTENSITY = "intensity";
-const std::string EmotionML::E_EXPECTATION = "expectation";
-const std::string EmotionML::E_OBJECT = "object";
+const std::string EmotionML::E_INFO = "info";
+const std::string EmotionML::E_REFERENCE = "reference";
+const std::string EmotionML::E_TRACE = "trace";
 
-	// Attributes
-const std::string EmotionML::A_SET = "set";
+// Attributes
+// Attributes of <emotion>
+const std::string EmotionML::A_CATEGORY_VOCABULARY = "category-set";
+const std::string EmotionML::A_DIMENSION_VOCABULARY = "dimension-set";
+const std::string EmotionML::A_APPRAISAL_VOCABULARY = "appraisal-set";
+const std::string EmotionML::A_ACTION_TENDENCY_VOCABULARY = "action-tendency-set";
+const std::string EmotionML::A_ID = "id";
+const std::string EmotionML::A_START = "start";
+const std::string EmotionML::A_END = "end";
+const std::string EmotionML::A_VERSION = "version";
+const std::string EmotionML::A_MODALITY = "modality";
+
+// Attributes of <category>, <dimension>, <appraisal>, <action-tendency> and <intensity>
+
 const std::string EmotionML::A_NAME = "name";
 const std::string EmotionML::A_VALUE = "value";
 const std::string EmotionML::A_CONFIDENCE = "confidence";
-const std::string EmotionML::A_TYPE = "type";
+
+// Attributes of <reference>
+const std::string EmotionML::A_URI = "uri";
+const std::string EmotionML::A_ROLE = "role";
+const std::string EmotionML::A_MEDIA_TYPE = "media-type";
+
+// Attribute of <trace>:
+const std::string EmotionML::A_FREQ = "freq";
+const std::string EmotionML::A_SAMPLES = "samples";
+
+// Values
+
+// Values of A_ROLE:
+const std::string EmotionML::V_EXPRESSED_BY = "expressedBy";
+const std::string EmotionML::V_EXPERIENCED_BY = "experiencedBy";
+const std::string EmotionML::V_TRIGGERED_BY = "triggeredBy";
+const std::string EmotionML::V_TARGETED_AT = "targetedAt";
+
+
+// Selected vocabularies
+
+// FSRE dimensions as in http://www.w3.org/TR/2010/WD-emotionml-20100729/#fsre-dimensions
+const std::string EmotionML::VOC_FSRE_DIMENSION_DEFINITION = "http://www.example.com/emotion/dimension/FSRE.xml"; // provisional
+const std::string EmotionML::VOC_FSRE_DIMENSION_VALENCE = "valence";
+const std::string EmotionML::VOC_FSRE_DIMENSION_POTENCY = "potency";
+const std::string EmotionML::VOC_FSRE_DIMENSION_AROUSAL = "arousal";
+const std::string EmotionML::VOC_FSRE_DIMENSION_UNPREDICTABILITY = "unpredictability";
+
+
+// SEMAINE-specific dimension vocabulary containing only one dimension, "intensity":
+const std::string EmotionML::VOC_SEMAINE_INTENSITY_DIMENSION_DEFINITON = "http://www.semaine-project.eu/emo/dimension/intensity.xml";
+const std::string EmotionML::VOC_SEMAINE_INTENSITY_DIMENSION_INTENSITY = "intensity";
+
+// SEMAINE-specific set of interest-related categories:
+const std::string EmotionML::VOC_SEMAINE_INTEREST_CATEGORY_DEFINITION = "http://www.semaine-project.eu/emo/category/interest.xml";
+const std::string EmotionML::VOC_SEMAINE_INTEREST_CATEGORY_BORED = "bored";
+const std::string EmotionML::VOC_SEMAINE_INTEREST_CATEGORY_NEUTRAL = "neutral";
+const std::string EmotionML::VOC_SEMAINE_INTEREST_CATEGORY_INTERESTED = "interested";
 
 } // namespace xml
 } // namespace datatypes
--- c++/src/semaine/datatypes/xml/EmotionML.h
+++ c++/src/semaine/datatypes/xml/EmotionML.h
@@ -15,32 +15,182 @@
 namespace semaine {
 namespace datatypes {
 namespace xml {
-
+/**
+ * This class provides string constants that match
+ * the Second Public Working Draft of EmotionML,
+ * http://www.w3.org/TR/2010/WD-emotionml-20100729/
+ * @author marc
+ *
+ */
 class EmotionML
 {
 public:
-	static const std::string version;
 	static const std::string namespaceURI;
+	static const std::string version;
 	
 	// Elements
+	static const std::string E_EMOTIONML;
 	static const std::string E_EMOTION;
 	static const std::string E_ROOT_TAGNAME;
 	static const std::string E_CATEGORY;
-	static const std::string E_DIMENSIONS;
-	static const std::string E_AROUSAL;
-	static const std::string E_VALENCE;
-//	static const std::string E_POTENCY;
-  static const std::string E_POWER;
-  static const std::string E_INTENSITY;
-  static const std::string E_EXPECTATION;
-	static const std::string E_OBJECT;
+	static const std::string E_DIMENSION;
+	static const std::string E_APPRAISAL;
+	static const std::string E_ACTION_TENDENCY;
+	static const std::string E_INTENSITY;
+	static const std::string E_INFO;
+	static const std::string E_REFERENCE;
+	static const std::string E_TRACE;
 
 	// Attributes
-	static const std::string A_SET;
+	// Attributes of <emotion>
+	static const std::string A_CATEGORY_VOCABULARY;
+	static const std::string A_DIMENSION_VOCABULARY;
+	static const std::string A_APPRAISAL_VOCABULARY;
+	static const std::string A_ACTION_TENDENCY_VOCABULARY;
+	static const std::string A_ID;
+	static const std::string A_START;
+	static const std::string A_END;
+	static const std::string A_VERSION;
+	static const std::string A_MODALITY;
+
+	// Attributes of <category>, <dimension>, <appraisal>, <action-tendency> and <intensity>
 	static const std::string A_NAME;
 	static const std::string A_VALUE;
 	static const std::string A_CONFIDENCE;
-	static const std::string A_TYPE;
+
+	// Attributes of <reference>
+	static const std::string A_URI;
+	static const std::string A_ROLE;
+	static const std::string A_MEDIA_TYPE;
+
+	// Attributes of <trace>
+	static const std::string A_FREQ;
+	static const std::string A_SAMPLES;
+
+	// Values
+
+	// Values of A_ROLE:
+	static const std::string V_EXPRESSED_BY;
+	static const std::string V_EXPERIENCED_BY;
+	static const std::string V_TRIGGERED_BY;
+	static const std::string V_TARGETED_AT;
+
+
+	// Selected vocabularies
+
+	// FSRE dimensions as in http://www.w3.org/TR/2010/WD-emotionml-20100729/#fsre-dimensions
+	static const std::string VOC_FSRE_DIMENSION_DEFINITION;
+	static const std::string VOC_FSRE_DIMENSION_VALENCE;
+	static const std::string VOC_FSRE_DIMENSION_POTENCY;
+	static const std::string VOC_FSRE_DIMENSION_AROUSAL;
+	static const std::string VOC_FSRE_DIMENSION_UNPREDICTABILITY;
+
+
+	// SEMAINE-specific dimension vocabulary containing only one dimension, "intensity":
+	static const std::string VOC_SEMAINE_INTENSITY_DIMENSION_DEFINITON;
+	static const std::string VOC_SEMAINE_INTENSITY_DIMENSION_INTENSITY;
+
+	// SEMAINE-specific set of interest-related categories:
+	static const std::string VOC_SEMAINE_INTEREST_CATEGORY_DEFINITION;
+	static const std::string VOC_SEMAINE_INTEREST_CATEGORY_BORED;
+	static const std::string VOC_SEMAINE_INTEREST_CATEGORY_NEUTRAL;
+	static const std::string VOC_SEMAINE_INTEREST_CATEGORY_INTERESTED;
+
+
+	/**
+	 * Convenience function to convert between the values as used in SEMAINE
+	 * and the FSRE scales used in EmotionML.
+	 */
+	inline static float semaineArousal2FSREArousal(float semaineArousal) {
+		return from11to01(semaineArousal);
+	}
+
+	/**
+	 * Convenience function to convert between the values as used in SEMAINE
+	 * and the FSRE scales used in EmotionML.
+	 */
+	inline static float fsreArousal2SemaineArousal(float fsreArousal) {
+		return from01to11(fsreArousal);
+	}
+
+	/**
+	 * Convenience function to convert between the values as used in SEMAINE
+	 * and the FSRE scales used in EmotionML.
+	 */
+	inline static float semaineValence2FSREValence(float semaineValence) {
+		return from11to01(semaineValence);
+	}
+
+	/**
+	 * Convenience function to convert between the values as used in SEMAINE
+	 * and the FSRE scales used in EmotionML.
+	 */
+	inline static float fsreValence2SemaineValence(float fsreValence) {
+		return from01to11(fsreValence);
+	}
+
+	/**
+	 * Convenience function to convert between the values as used in SEMAINE
+	 * and the FSRE scales used in EmotionML.
+	 */
+	inline static float semainePower2FSREPotency(float semainePower) {
+		return from11to01(semainePower);
+	}
+
+	/**
+	 * Convenience function to convert between the values as used in SEMAINE
+	 * and the FSRE scales used in EmotionML.
+	 */
+	inline static float fsrePotency2SemainePower(float fsrePotency) {
+		return from01to11(fsrePotency);
+	}
+
+	/**
+	 * Convenience function to convert between the values as used in SEMAINE
+	 * and the FSRE scales used in EmotionML.
+	 */
+	inline static float semaineExpectation2FSREUnpredictability(float semaineExpectation) {
+		return 1 - from11to01(semaineExpectation); // opposite polarity
+	}
+
+	/**
+	 * Convenience function to convert between the values as used in SEMAINE
+	 * and the FSRE scales used in EmotionML.
+	 */
+	inline static float fsreUnpredictability2SemaineExpectation(float fsreUnpredictability) {
+		return from01to11(fsreUnpredictability);
+	}
+
+private:
+	/**
+	 * Convert a value from the value range [-1;1] to the value range [0;1].
+	 * @param valueIn11
+	 * @return the value mapped to the target interval.
+	 */
+	inline static float from11to01(float valueIn11) {
+		float valueIn01 = (valueIn11 + 1.f) * 0.5f;
+		// and force into target interval
+		return _max(0.f, _min(1.f, valueIn01));
+	}
+
+	/**
+	 * Convert a value from the value range [0;1] to the value range [-1;1].
+	 * @param valueIn01
+	 * @return the value mapped to the target interval.
+	 */
+	inline static float from01to11(float valueIn01) {
+		float valueIn11 = valueIn01 * 2.f - 1.f;
+		// and force into target interval
+		return _max(-1.f, _min(1.f, valueIn11));
+	}
+
+	inline static float _max(float a, float b) {
+		return (a > b) ? a : b;
+	}
+
+	inline static float _min(float a, float b) {
+		return (a > b) ? b : a;
+	}
 };
 
 } // namespace xml
