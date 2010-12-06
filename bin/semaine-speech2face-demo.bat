3f8831eb96c4b73ec87f4f8c4e96b1f9d6e65ebe
mark.termaat <mark.termaat@2252c265-7fd8-49a5-b10e-6a9b7765d42e>
2010-12-06 15:58:28 +0000

    Fixes Ticket #167 & #162 (Agent suggesting to talk to itself) Adds config-files for a demo-version of SAL (Ticket #169). Start with bin/semaine-speech2face-demo or bin/semaine-speech2speech-demo
    git-svn-id: https://semaine.opendfki.de/repos/trunk@823 2252c265-7fd8-49a5-b10e-6a9b7765d42e

---
+++ bin/semaine-speech2face-demo.bat
@@ -0,0 +1,38 @@
+@echo off
+set BINDIR=%~dp0%
+
+set MARYDIR="%BINDIR%\..\MARY"
+
+if not exist %MARYDIR% (
+  echo.MARY directory not found in %MARYDIR%. Cannot start.
+  goto end
+)
+
+set JARDIR="%BINDIR%..\java\lib"
+
+if not exist %JARDIR%\semaine.jar (
+  echo.No semaine.jar -- you need to do 'ant jars' in the java folder first!
+  goto end
+)
+
+if %1%a==a (
+  set CONFIG="%BINDIR%..\java\config\speech2face-demo.config"
+) else (
+  set CONFIG=%1%
+)
+
+if not %CMS_URL%a==a (
+  echo.Connecting to JMS server at %CMS_URL%
+  set JMS_URL_SETTING="-Djms.url=%CMS_URL%"
+)
+
+
+
+echo.Starting SEMAINE system as: 'java -Xmx1500m %JMS_URL_SETTING% -classpath %JARDIR%\semaine.jar;%JARDIR%\semaine-mary.jar;%JARDIR%\semaine-dialogue.jar -Dmary.base="%MARYDIR%" eu.semaine.system.ComponentRunner %CONFIG%'
+
+java -Xmx1500m -Dlog4j.logger.semaine=DEBUG,logfile,stderr %JMS_URL_SETTING% -classpath %JARDIR%\semaine.jar;%JARDIR%\semaine-mary.jar;%JARDIR%\semaine-dialogue.jar -Dmary.base=%MARYDIR% eu.semaine.system.ComponentRunner %CONFIG%
+
+:: goto target:
+
+:end
+set /P DUMMY=Press return to continue...
