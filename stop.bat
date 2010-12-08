9220f96fbeede340a0e3643347ce91f89ccfc8d8
masc01 <masc01@2252c265-7fd8-49a5-b10e-6a9b7765d42e>
2010-12-08 15:04:22 +0000

    RC2
    git-svn-id: https://semaine.opendfki.de/repos/trunk@846 2252c265-7fd8-49a5-b10e-6a9b7765d42e

--- stop.bat
+++ stop.bat
@@ -2,10 +2,6 @@
 
 :: Locations of installed software:
 set SEMAINEDIR=%~dp0%
-set ACTIVEMQDIR=%SEMAINEDIR%apache-activemq-5.3.0
-
-FOR /F "tokens=2" %%n IN ('TASKLIST /NH /FI "WINDOWTITLE eq speech2face - bin\semaine-speech2face.bat"' ) DO SET PID=%%n
-taskkill /PID %PID%
 
 taskkill /f /im Active_ActionSelection.exe
 taskkill /f /im Active_BehaviorPlanner.exe
@@ -18,5 +14,7 @@ taskkill /f /im SEMAINExtract.exe
 
 taskkill /f /im VideoFeatureExtractorConsole.exe
 
-if exist %ACTIVEMQDIR% start /MIN /D %ACTIVEMQDIR%\bin activemq-admin.bat stop
+
+FOR /F "tokens=2" %%n IN ('TASKLIST /NH /FI "WINDOWTITLE eq speech2face - bin\semaine-speech2face.bat"' ) DO SET PID=%%n
+taskkill /PID %PID%
 
