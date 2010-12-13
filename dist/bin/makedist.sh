98bebf79dc2fbf134af2821a03a2b619f5f32f45
masc01 <masc01@2252c265-7fd8-49a5-b10e-6a9b7765d42e>
2010-12-13 11:09:12 +0000

    More fixes needed for makedist scripts.
    git-svn-id: https://semaine.opendfki.de/repos/trunk@864 2252c265-7fd8-49a5-b10e-6a9b7765d42e

--- dist/bin/makedist.sh
+++ dist/bin/makedist.sh
@@ -11,7 +11,9 @@ if [ -z "$PACKAGE" ] ; then
 fi
 
 FILELIST="$ROOTDIR/dist/releasefiles-$PACKAGE.txt"
-if [ ! -e "$FILELIST" ] ; then
+if [ -e "$FILELIST" ] ; then
+  echo "Creating package from $FILELIST"
+else
   echo "Not a known package: $PACKAGE"
   exit 1
 fi
@@ -22,24 +24,29 @@ INST_ROOTDIR="SEMAINE-$VERSION"
 
 mkdir -p "$ROOTDIR/dist/$INST_ROOTDIR"
 DISTFILE="$ROOTDIR/dist/$INST_ROOTDIR/$INST_ROOTDIR-$PACKAGE.zip"
-if [ -x "$DISTFILE" ] ; then
+if [ -e "$DISTFILE" ] ; then
+  echo "Removing existing $DISTFILE"
   rm "$DISTFILE"
 fi
 
 TMPDIR="$ROOTDIR/dist/tmp"
 mkdir -p "$TMPDIR"
-if [ -x "$TMPDIR/$INST_ROOTDIR" ] ; then
+if [ -e "$TMPDIR/$INST_ROOTDIR" ] ; then
+  echo "Removing existing $TMPDIR/$INST_ROOTDIR"
   rm "$TMPDIR/$INST_ROOTDIR"
 fi
 ln -s "$ROOTDIR" "$TMPDIR/$INST_ROOTDIR"
 DISTFILELIST="$TMPDIR/distfiles-$PACKAGE.txt"
 if [ -e "$DISTFILELIST" ] ; then
+  echo "Removing existing $DISTFILELIST"
   rm "$DISTFILELIST"
 fi
 
 # Prepare the actual list of files from $FILELIST:
 # prepend $INST_ROOTDIR in front of each file, 
 # and expand directories.
+
+echo "Creating full file list in $DISTFILELIST"
 
 # In argument of for loop, split at newline only,
 # in order to allow file names including spaces:
@@ -50,13 +57,15 @@ IFS='
 (cd "$TMPDIR" 
 for f in `cat "$FILELIST"` ; do
   if [ -d "$INST_ROOTDIR/$f" ] ; then
-    find "$INST_ROOTDIR/$f" -print | egrep -v "^.svn|/.svn|~$" >> "$DISTFILELIST"
+    find "$INST_ROOTDIR/$f" -print | egrep -v "^\.svn|/\.svn|~$|/\.libs|\.o$|\.lo$|\.loT$|.DS_Store|/\.deps|\.a$|/\.dirstamp$" >> "$DISTFILELIST" 
   elif [ -f "$INST_ROOTDIR/$f" ] ; then
     echo "$INST_ROOTDIR/$f" >> "$DISTFILELIST"
   else
     echo "Warning: $ROOTDIR/$f does not exist."
   fi
 done
+
+echo "Creating $DISTFILE"
 
 cat "$DISTFILELIST" | zip -q "$DISTFILE" -@
 )
